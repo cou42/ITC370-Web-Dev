5164c7dbb4a0a7670f11e9c46bf4181b277351de
Courtney Fuerstenau <cou42@live.missouristate.edu>
2021-11-15 20:48:45 -0600

    update

---
+++ Image Swapping/scripts.js
@@ -0,0 +1,32 @@
+function changeImage(a) {
+    document.getElementById("img").src=a;
+}
+
+document.getElementById("img").addEventListener("dblclick", function() {
+    if(thumb.src = "dog1.jpg"){
+        thumb.src = "cat1.jpg";}
+        else{
+            thumb.src = "dog1.jpg"
+        }
+
+    if(thumb1.src = "dog2.jpg"){
+        thumb1.src = "cat2.jpg";}
+        else{
+            thumb1.src = "dog2.jpg"
+        }
+        if(thumb2.src = "dog3.jpg"){
+            thumb2.src = "cat3.jpg";}
+            else {
+                thumb2.src = "dog3.jpg"
+            }
+
+            if(thumb3.src = "dog4.jpg"){
+                thumb3.src = "cat4.jpg";}
+            else {
+                thumb3.src = "dog4.jpg"
+            }
+ });
+
+
+
+ 
